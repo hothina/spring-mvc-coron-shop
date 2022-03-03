9f1aaf50af31fbfbbcd31cb979b5418f85e86778
hothina <88428484+hothina@users.noreply.github.com>
2022-03-03 09:25:39 +0700

    Bai tap

--- src/main/java/com/cg/controller/cp/CPProductController.java
+++ src/main/java/com/cg/controller/cp/CPProductController.java
@@ -99,7 +99,6 @@ public class CPProductController {
             streamLocal.close();
 
             product.setAvatar(fileName);
-
             productService.save(product);
         } catch (IOException e) {
             e.printStackTrace();
